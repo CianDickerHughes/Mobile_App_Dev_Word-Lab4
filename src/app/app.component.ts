931c3605a78518304c7f0017112ed7ba51bf2a7c
CianDickerHughes <G00415413@atu.ie>
2024-02-29 15:38:21 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'data-binding-app';
+  starClick:boolean = true;
+  count:number = 0;
+
+  onButtonClick() {
+    this.count++;
+  }
+
+  onImgClick() {
+
+    this.starClick = !this.starClick;
+
+    /*if(this.starClick === false) {
+      this.starClick = true
+    }
+    else {
+      this.starClick = false;
+    } */
+  }
 }
